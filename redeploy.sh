81ec839ca4b70d4b6b3242dd6f9bfa9bf7fdda42
gangerang <ajallchin@gmail.com>
2025-02-12 20:45:37 +1100

    fix redeploy

--- redeploy.sh
+++ redeploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# Set the container name and image name
-CONTAINER_NAME="ohlrv_container"
-IMAGE_NAME="ohlrv_image"
+# Set the container name, image name, and port
+CONTAINER_NAME="ohlrv"
+IMAGE_NAME="ohlrv"
+PORT="5000" # Change this to your desired port
 
 echo "Stopping and removing existing container..."
 docker stop $CONTAINER_NAME
@@ -14,7 +15,7 @@ docker rmi $IMAGE_NAME
 echo "Building new image..."
 docker build -t $IMAGE_NAME .
 
-echo "Starting new container..."
-docker run -d --name $CONTAINER_NAME $IMAGE_NAME
+echo "Starting new container with port mapping..."
+docker run -d -p $PORT:$PORT --name $CONTAINER_NAME $IMAGE_NAME
 
-echo "Deployment complete!"
+echo "Deployment complete! Container is running on port $PORT"
